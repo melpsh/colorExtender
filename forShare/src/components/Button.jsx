8a2d9df8b02dcef69cbff02e20ccc68f9a7d6152
Melina Pashna <melinaapashna@gmail.com>
2024-03-16 13:21:54 -0700

    btn works properly

---
+++ forShare/src/components/Button.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './Button.css';
+
+const Button = () => {
+    const [isActive, setIsActive] = useState(false);
+    const [boxShadowColor, setBoxShadowColor] = useState('transparent');
+
+    const handleClick = () => {
+        setIsActive(true);
+
+        const hexColor = generateHexColor();
+        setBoxShadowColor(hexColor);
+
+        // Clear active class after the animation ends
+        setTimeout(() => {
+            setIsActive(false);
+        }, 3000); // Match the duration of the animation
+    };
+
+    const generateHexColor = () => {
+        let hexColor = '#';
+        const colors = ['00', '33', '66', '99', 'CC', 'FF'];
+        for (let i = 0; i < 3; i++) {
+            hexColor += colors[Math.floor(Math.random() * colors.length)];
+        }
+        return hexColor;
+    };
+
+    return (
+        <div className="container">
+            <button
+            className={isActive ? 'pulse_btn active' : 'pulse_btn'}
+            onClick={handleClick}
+            style={{
+                boxShadow: `0 0 0 100vw ${boxShadowColor}`
+            }}
+            >
+            Change Color
+            </button>
+        </div>
+    );
+}
+
+export default Button;
