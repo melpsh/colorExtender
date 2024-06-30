18cd2f08b66dbd9303703f39777e6c204327c04e
Melina Pashna <melinaapashna@gmail.com>
2024-06-29 18:59:43 -0700

    color Pallette export

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import "./colorPalette.css";
 import { MdClose } from "react-icons/md";
+import html2canvas from "html2canvas";
+import "./colorPalette.css";
 
 const ColorPalette = ({ colors, onRemoveColor, onClearPalette }) => {
   const [hoveredColor, setHoveredColor] = useState(null);
@@ -10,25 +11,41 @@ const ColorPalette = ({ colors, onRemoveColor, onClearPalette }) => {
     onRemoveColor(color);
   };
 
+  const handleExportPalette = () => {
+    const paletteContainer = document.getElementById('palette-container');
+    html2canvas(paletteContainer, { backgroundColor: "#FFFFFF" }).then(canvas => {
+      const dataUri = canvas.toDataURL('image/jpeg');
+      const exportFileDefaultName = 'color_palette.jpg';
+      const linkElement = document.createElement('a');
+      linkElement.setAttribute('href', dataUri);
+      linkElement.setAttribute('download', exportFileDefaultName);
+      linkElement.click();
+    });
+  };
+
   return (
-    <div className="color-palette">
-      {colors.map((color, index) => (
-        <div
-          key={index}
-          className="color-square"
-          style={{ backgroundColor: color }}
-        >
+    <div className="color-palette-container">
+      <div id="palette-container" className="color-palette">
+        {colors.map((color, index) => (
           <div
-            className="remove-icon"
-            onClick={() => handleRemoveColorClick(color)}
+            key={index}
+            className="color-square"
+            style={{ backgroundColor: color }}
+            onMouseEnter={() => setHoveredColor(color)}
+            onMouseLeave={() => setHoveredColor(null)}
           >
-            <MdClose />
+            <div className="remove-icon" onClick={() => handleRemoveColorClick(color)}>
+              <MdClose />
+            </div>
+            {hoveredColor === color && <div className="hover-alert">Hovered</div>}
           </div>
-          {hoveredColor === color && <div className="hover-alert">Hovered</div>}
-        </div>
-      ))}
+        ))}
+      </div>
       <button className="clear-palette-btn" onClick={onClearPalette}>
         Clear the palette
+      </button>
+      <button className="export-palette-btn" onClick={handleExportPalette}>
+        Export the palette
       </button>
     </div>
   );
