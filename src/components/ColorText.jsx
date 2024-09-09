acc48e7e25bf9c5cdfdb288d2feac35302756db7
Melina Pashna <melinaapashna@gmail.com>
2024-09-08 18:04:39 -0700

    colorText improvments

--- src/components/ColorText.jsx
+++ src/components/ColorText.jsx
@@ -6,15 +6,15 @@ const ColorText = ({ backgroundColor }) => {
     const r = parseInt(hex.substring(1, 3), 16);
     const g = parseInt(hex.substring(3, 5), 16);
     const b = parseInt(hex.substring(5, 7), 16);
-    return `(${r}, ${g}, ${b})`;
+    return { r, g, b };
   };
 
   const rgbColor = hexToRgb(backgroundColor);
 
   const textColor = (hexColor) => {
-    const rgb = hexToRgb(hexColor);
-    const luminance = (0.2126 * rgb.r + 0.7152 * rgb.g + 0.0722 * rgb.b) / 255;
-    return luminance > 0.5 ? "black" : "white";
+    const { r, g, b } = hexToRgb(hexColor);
+    const luminance = (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255;
+    return luminance > 0.5 ? "black" : "white"; // Use black text for light backgrounds and white for dark backgrounds
   };
 
   const textStyle = {
@@ -27,7 +27,7 @@ const ColorText = ({ backgroundColor }) => {
         BackgroundColor: {backgroundColor}
       </h1>
       <h1 className="colorRGB text-with-border" style={textStyle}>
-        RGB : {rgbColor}
+        RGB: ({rgbColor.r}, {rgbColor.g}, {rgbColor.b})
       </h1>
     </div>
   );
